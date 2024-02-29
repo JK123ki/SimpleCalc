632e11eed7aa44fd7f3e783250be980cd4e3497f
JK123ki <160002390+JK123ki@users.noreply.github.com>
2024-02-29 12:11:36 +0300

    Update calc.c

--- calc.c
+++ calc.c
@@ -4,18 +4,6 @@
 
 int main() {
     // Declare variables
-    float num1, num2, sum, diff;
-
-    // Get user input
-    printf("Enter the first number: ");
-    scanf("%f", &num1);
-
-    printf("Enter the second number: ");//ENE212-0181/2021
-//JACOB KIRIGHA
-#include <stdio.h>
-
-int main() {
-    // Declare variables
     float num1, num2, sum, diff,mult,div;
 
     // Get user input
@@ -39,23 +27,4 @@ int main() {
 
     return 0;
 }
-    scanf("%f", &num2);
-
-    // Perform addition and subtraction separately
-    sum = num1 + num2;
-    diff = num1 - num2;
-
-    // Display results
-    printf("Sum: %.2f\n", sum);
-    printf("Difference: %.2f\n", diff);
-
-    // Perform addition and subtraction at the same time
-    sum = num1 + num2;
-    diff = num1 - num2;
-
-    // Display results
-    printf("Sum and Difference at the same time:\n");
-    printf("Sum: %.2f, Difference: %.2f\n", sum, diff);
-
-    return 0;
-}
+   
